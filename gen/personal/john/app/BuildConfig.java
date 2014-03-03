98a80f71f82be41522b0ca59f9ca6a7671905126
john <pcelsius000@gmail.com>
2014-03-04 01:38:12 +0900

    不要ファイルの削除
    不要ファイルの削除

--- gen/personal/john/app/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package personal.john.app;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/personal/john/GeoSearcher/RakutenClientReceiver.java
+++
@@ -1,8 +0,0 @@
-package personal.john.GeoSearcher;
-
-import java.util.ArrayList;
-
-public interface RakutenClientReceiver {
-	public void receiveHotel(ArrayList<HotelInfo> infoList);
-	public void receiveError(int id);
-}
